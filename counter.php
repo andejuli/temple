f22d41b90e2481fde39117855d7f7fab3fd0d2a8
andejuli <techtrek@hotmail.com>
2025-05-11 19:53:31 -0600

    take out backend files

--- counter.php
+++
@@ -1,8 +0,0 @@
-<?php
-  header("Access-Control-Allow-Origin: *");
-  $filename = 'counter.txt';
-  $counter = (int)file_get_contents($filename);
-  $counter++;
-  file_put_contents($filename, $counter);
-  echo $counter;
-?>
